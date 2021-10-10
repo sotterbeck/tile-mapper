20c77e8ee9341b166f93b4e98d1f46a05598b62b
SimBuildings <simon.otterbeck@gmail.com>
2021-10-10 13:13:29 +0200

    Initial commit

---
+++ src/main/java/de/simbuildings/tilemapper/Main.java
@@ -0,0 +1,10 @@
+package de.simbuildings.tilemapper;
+
+/**
+ * Created by SimBuildings on 09.10.21 at 23:44
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
