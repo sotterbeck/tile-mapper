a7d47a2374d696a01f5f148b0fcdcefeb7766533
SimBuildings <simon.otterbeck@gmail.com>
2021-10-11 23:55:17 +0200

    * [WIP] test class for ImageSpliter

---
+++ src/test/java/de/simbuildings/tilemapper/tile/ImageSpliterTest.java
@@ -0,0 +1,20 @@
+package de.simbuildings.tilemapper.tile;
+
+import de.simbuildings.tilemapper.image.SquareImageResolution;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by SimBuildings on 11.10.21 at 19:10
+ */
+class ImageSpliterTest {
+
+    @Test
+    void split() {
+
+    }
+}
