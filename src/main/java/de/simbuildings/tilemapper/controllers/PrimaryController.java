6e195ea74ae56b3c224a4acd61eddc8845c9d4e9
SimBuildings <simon.otterbeck@gmail.com>
2021-10-16 17:30:51 +0200

    disable buttons until valid image file is choosen. Implementation of resolution calculation for combobox.

--- src/main/java/de/simbuildings/tilemapper/controllers/PrimaryController.java
+++ src/main/java/de/simbuildings/tilemapper/controllers/PrimaryController.java
@@ -1,10 +1,18 @@
 package de.simbuildings.tilemapper.controllers;
 
+import de.simbuildings.tilemapper.image.ImageResolution;
+import de.simbuildings.tilemapper.image.SquareImageResolution;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by SimBuildings on 15.10.21 at 18:34
@@ -12,18 +20,54 @@ import java.io.File;
 public class PrimaryController {
 
     @FXML
-    private Button importButton;
+    private Button importButton, exportButton;
+    @FXML
+    private TextField blockTextField;
+    @FXML
+    private ComboBox<Integer> resolutionComboBox;
 
     private FileChooser fileChooser;
-    private File imageFile;
+    private BufferedImage originalImage;
 
-    public void importFile() {
+    public void handleImportButtonAction(ActionEvent event) throws IOException {
+        File imageFile;
+
         fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(
                 new FileChooser.ExtensionFilter("PNG Image File", "*.png"));
         imageFile = fileChooser.showOpenDialog(importButton.getScene().getWindow());
-        if (imageFile != null) {
-            System.out.println("image file selected");
+
+        if (imageFile != null && ImageIO.read(imageFile) != null) {
+            originalImage = ImageIO.read(imageFile);
+            enableForm();
+        } else {
+            disableForm();
         }
     }
+
+
+    public void handleExportButtonAction(ActionEvent event) {
+    }
+
+
+    // TODO form  (nested) class ? - Single Responsibility Priciple
+    private void enableForm() {
+        ImageResolution imageResolution = new ImageResolution(originalImage);
+        if (imageResolution.isPowerOfTwo()) {
+            resolutionComboBox.setDisable(false);
+            blockTextField.setDisable(false);
+
+            for (SquareImageResolution res :
+                    imageResolution.getValuesPowerOfTwoUntilRes()) {
+                resolutionComboBox.getItems().add(res.getHeight());
+            }
+        }
+    }
+    private void disableForm() {
+        resolutionComboBox.setDisable(true);
+        blockTextField.setDisable(true);
+
+        resolutionComboBox.getItems().clear();
+    }
+
 }
