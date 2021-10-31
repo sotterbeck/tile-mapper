56093cf1aa8d1ff397b8182a1186ac5b058fbf27
SimBuildings <simon.otterbeck@gmail.com>
2021-10-31 12:36:57 +0100

    reverted to previous implementation

--- src/test/java/de/simbuildings/tilemapper/tile/ImageSpliterTest.java
+++ src/test/java/de/simbuildings/tilemapper/tile/ImageSpliterTest.java
@@ -3,16 +3,12 @@ package de.simbuildings.tilemapper.tile;
 import de.simbuildings.tilemapper.image.SquareImageResolution;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -20,30 +16,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 /**
  * Created by SimBuildings on 11.10.21 at 19:10
  */
-
 class ImageSpliterTest {
 
     private final String path = "src/test/resources/image/";
-    private final InputStream workingImage = getClass().getClassLoader().getResourceAsStream("image/tile_sample_working.png");
-    private final InputStream failingImage = getClass().getClassLoader().getResourceAsStream("image/tile_sample_failing.png");
-    private static final URL splitDir = ImageSpliterTest.class.getClassLoader().getResource("image/split");
-
     private ImageSpliter underTest;
-
-    @BeforeAll
-    static void beforeAll() throws IOException {
-        FileUtils.forceMkdir(new File(Objects.requireNonNull(splitDir).getFile()));
-    }
 
     @AfterEach
     void tearDown() throws IOException {
-        FileUtils.cleanDirectory(new File(Objects.requireNonNull(splitDir).getFile()));
+        FileUtils.cleanDirectory(new File(path + "split/"));
     }
 
     @Test
     void shouldSplitImage() throws IOException {
         // given
-        BufferedImage image = ImageIO.read(Objects.requireNonNull(workingImage));
+        BufferedImage image = ImageIO.read(new File(path + "tile_sample_working.png"));
         SquareImageResolution targetResoltion = new SquareImageResolution(64);
 
         // when
@@ -61,7 +47,7 @@ class ImageSpliterTest {
         SquareImageResolution targetResoltion = new SquareImageResolution(64);
 
         // when
-        BufferedImage image = ImageIO.read(Objects.requireNonNull(failingImage));
+        BufferedImage image = ImageIO.read(new File(path + "tile_sample_failing.png"));
 
         // then
         assertThatThrownBy(() -> underTest = new ImageSpliter(image, targetResoltion)).isInstanceOf(RuntimeException.class);
@@ -70,17 +56,17 @@ class ImageSpliterTest {
     @Test
     void shouldExportSplitImage() throws IOException {
         // given
-        BufferedImage image = ImageIO.read(Objects.requireNonNull(workingImage));
+        BufferedImage image = ImageIO.read(new File(path + "tile_sample_working.png"));
         SquareImageResolution targetResoltion = new SquareImageResolution(64);
+        String destDir = path + "split/";
 
         // when
         underTest = new ImageSpliter(image, targetResoltion);
         underTest.split();
-        underTest.exportTiles(Objects.requireNonNull(splitDir).getPath() + "/");
-        System.out.println(splitDir.getPath());
+        underTest.save(destDir);
 
         // then
-        assertThat(new File(splitDir.getPath() + "/")).isNotEmptyDirectory();
+        assertThat(new File(destDir)).isNotEmptyDirectory();
     }
 
 }
