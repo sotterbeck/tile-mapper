648dafdf17158bb74b34e88d461316a05b0e3111
simbuildings <simon.otterbeck@gmail.com>
2022-02-12 23:23:46 +0100

    added import functionality and form disabled property binding

--- src/main/java/de/simbuildings/tilemapper/ui/controllers/PrimaryController.java
+++ src/main/java/de/simbuildings/tilemapper/ui/controllers/PrimaryController.java
@@ -1,13 +1,17 @@
 package de.simbuildings.tilemapper.ui.controllers;
 
 import de.simbuildings.tilemapper.ui.models.TileModel;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,6 +19,7 @@ import java.util.ResourceBundle;
  * Created by SimBuildings on 12.02.22 at 21:32
  */
 public class PrimaryController implements Initializable {
+    private final TileModel tileModel = new TileModel();
     @FXML
     public Button importButton;
     @FXML
@@ -23,12 +28,44 @@ public class PrimaryController implements Initializable {
     public ComboBox<Integer> resolutionComboBox;
     @FXML
     public TextField blockTextField;
-
-    private final TileModel tileModel = new TileModel();
+    @FXML
+    public Button exportButton;
 
     @Override
-
     public void initialize(URL location, ResourceBundle resources) {
-        blockTextField.textProperty().bind(tileModel.blockNameProperty());
+        blockTextField.disableProperty()
+                .bind(tileModel.originalImageProperty().isNull());
+        resolutionComboBox.disableProperty()
+                .bind(tileModel.originalImageProperty().isNull());
+
+        exportButton.disableProperty().bind(
+                blockTextField.textProperty().isEmpty()
+        );
+
+        tileModel.blockNameProperty().bind(blockTextField.textProperty());
+    }
+
+    @FXML
+    private void handleImport(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG image file", "*.png"));
+
+        File originalImage = fileChooser.showOpenDialog(importButton.getScene().getWindow());
+        if (originalImage != null) {
+            setOriginalImage(originalImage);
+        }
+    }
+
+    private void setOriginalImage(File originalImage) {
+        try {
+            tileModel.setOriginalImage(originalImage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleExport(ActionEvent actionEvent) {
+
     }
 }
