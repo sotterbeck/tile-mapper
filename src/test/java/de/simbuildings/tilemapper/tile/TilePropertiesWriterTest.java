476d93e8b5b52ec06da3f39a247a99fc2b13ae47
simbuildings <simon.otterbeck@gmail.com>
2022-03-20 14:54:54 +0100

    test: test for exception and assert that file exists using temporary directory

--- src/test/java/de/simbuildings/tilemapper/tile/TilePropertiesWriterTest.java
+++ src/test/java/de/simbuildings/tilemapper/tile/TilePropertiesWriterTest.java
@@ -2,32 +2,38 @@ package de.simbuildings.tilemapper.tile;
 
 import de.simbuildings.tilemapper.image.SquareImageResolution;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
-import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 /**
  * Created by SimBuildings on 31.10.21 at 16:21
  */
 class TilePropertiesWriterTest {
-    private static final String PATH = "src/test/resources/image/split";
+
     private TilePropertiesWriter underTest;
+
+    @TempDir
+    private File tempDir;
 
     @Test
     void shouldWritePropertiesFile() {
         // given
         String block = "minecraft:stone";
-        TileGrid grid = new TileGrid(new SquareImageResolution(128), new SquareImageResolution(32));
-
-        // when
+        SquareImageResolution originalResolution = new SquareImageResolution(128);
+        SquareImageResolution targetResolution = new SquareImageResolution(32);
+        TileGrid grid = new TileGrid(originalResolution, targetResolution);
         underTest = new TilePropertiesWriter(grid, block);
 
+        // when
+        Throwable thrown = catchThrowable(() -> underTest.write(tempDir));
+
         // then
-        try {
-            underTest.write(new File(PATH));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        assertThat(thrown).isNull();
+        assertThat(tempDir).isDirectoryContaining(file -> file.getName().equals("stone.properties"));
     }
 
     // TODO: test for reading properties file and check if values are as expected
