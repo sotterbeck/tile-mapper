ff3c22adde5c8aa0f80f31ef64f2b6dc9ffcaf38
simbuildings <simon.otterbeck@gmail.com>
2022-03-26 15:21:27 +0100

    chore: add new dependencies to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,27 @@
 module de.simbuildings.tilemapper {
+    requires java.prefs;
+
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.swing;
+
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+    requires net.harawata.appdirs;
 
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.feather;
     requires thumbnailator;
 
     opens de.simbuildings.tilemapper.ui.controllers to javafx.fxml;
+    opens de.simbuildings.tilemapper.resourcepack to com.fasterxml.jackson.databind;
+
     exports de.simbuildings.tilemapper.ui.models;
     exports de.simbuildings.tilemapper;
 
     exports de.simbuildings.tilemapper.common;
     exports de.simbuildings.tilemapper.tile;
     exports de.simbuildings.tilemapper.image;
+    exports de.simbuildings.tilemapper.resourcepack;
 }
