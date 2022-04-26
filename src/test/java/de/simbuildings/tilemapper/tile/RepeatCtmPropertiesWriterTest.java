ad065132dbe9e25439a89233e772fd8372c01541
simbuildings <simon.otterbeck@gmail.com>
2022-04-26 17:54:58 +0200

    test: add tests for export conflict methods in CtmPropertiesWriter

--- src/test/java/de/simbuildings/tilemapper/tile/RepeatCtmPropertiesWriterTest.java
+++ src/test/java/de/simbuildings/tilemapper/tile/RepeatCtmPropertiesWriterTest.java
@@ -5,6 +5,7 @@ import de.simbuildings.tilemapper.image.ImageResolution;
 import de.simbuildings.tilemapper.image.SquareImageResolution;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -12,6 +13,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Properties;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -71,5 +73,52 @@ class RepeatCtmPropertiesWriterTest {
                 .containsEntry("width", String.valueOf(grid.getWidth()))
                 .containsEntry("method", "repeat")
                 .containsEntry("tiles", "0-%d".formatted(grid.getTileAmount() - 1));
+    }
+
+    @Nested
+    @DisplayName("has Conflicts")
+    class HasConflicts {
+        @Test
+        @DisplayName("should return false when file does not exists")
+        void shouldReturnFalse_WhenFileDoesNotExists() {
+            // given
+            underTest = new RepeatCtmPropertiesWriter(block, grid);
+
+            // when
+            boolean result = underTest.hasConflict(tempDir);
+
+            // then
+            assertThat(result).isFalse();
+        }
+
+        @Test
+        @DisplayName("should return true when file already exists")
+        void shouldReturnTrue_WhenFileAlreadyExists() throws IOException {
+            // given
+            underTest = new RepeatCtmPropertiesWriter(block, grid);
+            underTest.export(tempDir);
+
+            // when
+            boolean result = underTest.hasConflict(tempDir);
+
+            // then
+            assertThat(result).isTrue();
+        }
+    }
+
+    @Test
+    @DisplayName("Should get conflict files")
+    void getConflictFiles_ShouldReturnExportedFile_WhenConflict() throws IOException {
+        // given
+        underTest = new RepeatCtmPropertiesWriter(block, grid);
+        underTest.export(tempDir);
+
+        // when
+        Set<Path> result = underTest.getConflictFiles(tempDir);
+
+        // then
+        assertThat(result)
+                .hasSize(1)
+                .contains(tempDir.resolve(fileName));
     }
 }
