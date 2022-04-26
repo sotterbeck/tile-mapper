4d020613a2d5fb6f5d2192ffa8c71466cab3b274
simbuildings <simon.otterbeck@gmail.com>
2022-04-26 17:16:15 +0200

    test: add test for isPowerOfTwo in ImageResolution

--- src/test/java/de/simbuildings/tilemapper/image/ImageResolutionTest.java
+++ src/test/java/de/simbuildings/tilemapper/image/ImageResolutionTest.java
@@ -59,4 +59,30 @@ class ImageResolutionTest {
         // then
         assertThat(result).isTrue();
     }
+
+    @Test
+    @DisplayName("should check if resolution is power of two when correct")
+    void isPowerOfTwo_ShouldReturnTrue_WhenResolutionIsPowerOfTwo() {
+        // given
+        ImageResolution resolution = new ImageResolution(128, 128);
+
+        // when
+        boolean actual = resolution.isPowerOfTwo();
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("should check if resolution is power of two when false")
+    void isPowerOfTwo_ShouldReturnFalse_WhenResolutionIsNotPowerOfTwo() {
+        // given
+        ImageResolution resolution = new ImageResolution(123, 342);
+
+        // when
+        boolean actual = resolution.isPowerOfTwo();
+
+        // then
+        assertThat(actual).isFalse();
+    }
 }
