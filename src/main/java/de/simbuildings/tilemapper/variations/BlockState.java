a3a9b5521a0c83376b66c3e17ed3f2efd45d445d
simbuildings <simon.otterbeck@gmail.com>
2022-05-05 19:32:37 +0200

    fix: return immutable map from variants

--- src/main/java/de/simbuildings/tilemapper/variations/BlockState.java
+++ src/main/java/de/simbuildings/tilemapper/variations/BlockState.java
@@ -3,10 +3,7 @@ package de.simbuildings.tilemapper.variations;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonGetter;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class BlockState {
     private final Map<String, Set<Variant>> variantMap;
@@ -31,7 +28,7 @@ public class BlockState {
     @JsonGetter("variants")
     @JsonFormat(with = JsonFormat.Feature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED)
     public Map<String, Set<Variant>> variants() {
-        return variantMap;
+        return Collections.unmodifiableMap(variantMap);
     }
 
     public static class Builder {
