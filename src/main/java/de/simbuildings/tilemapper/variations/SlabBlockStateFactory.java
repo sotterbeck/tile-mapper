7562787b3b27bcd8cbd44219fd172042603ebcfd
simbuildings <simon.otterbeck@gmail.com>
2022-05-08 19:00:08 +0200

    fix: sort variant array

--- src/main/java/de/simbuildings/tilemapper/variations/SlabBlockStateFactory.java
+++ src/main/java/de/simbuildings/tilemapper/variations/SlabBlockStateFactory.java
@@ -1,7 +1,9 @@
 package de.simbuildings.tilemapper.variations;
 
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class SlabBlockStateFactory implements Supplier<BlockState> {
@@ -9,12 +11,6 @@ public class SlabBlockStateFactory implements Supplier<BlockState> {
     private final Set<Variant.Builder> bottomVariants;
     private final Set<Variant.Builder> doubleVariants;
     private final Set<Variant.Builder> topVariants;
-
-    public SlabBlockStateFactory(Set<Variant.Builder> bottomVariants, Set<Variant.Builder> doubleVariants, Set<Variant.Builder> topVariants) {
-        this.bottomVariants = bottomVariants;
-        this.doubleVariants = doubleVariants;
-        this.topVariants = topVariants;
-    }
 
     public SlabBlockStateFactory(Set<Variant.Builder> variants) {
         this.bottomVariants = variants;
@@ -27,15 +23,19 @@ public class SlabBlockStateFactory implements Supplier<BlockState> {
                 .namedVariants("type=bottom", bottomVariants.stream()
                         .map(builder -> builder.modelType(ModelType.SLAB))
                         .map(Variant.Builder::build)
-                        .collect(Collectors.toUnmodifiableSet()))
+                        .collect(sortedSetCollector()))
                 .namedVariants("type=double", doubleVariants.stream()
                         .map(builder -> builder.modelType(ModelType.BLOCK))
                         .map(Variant.Builder::build)
-                        .collect(Collectors.toUnmodifiableSet()))
+                        .collect(sortedSetCollector()))
                 .namedVariants("type=top", topVariants.stream()
                         .map(builder -> builder.modelType(ModelType.SLAB_TOP))
                         .map(Variant.Builder::build)
-                        .collect(Collectors.toUnmodifiableSet()))
+                        .collect(sortedSetCollector()))
                 .build();
+    }
+
+    private Collector<Variant, ?, TreeSet<Variant>> sortedSetCollector() {
+        return Collectors.toCollection(TreeSet::new);
     }
 }
--- src/test/java/de/simbuildings/tilemapper/variations/SlabBlockStateTest.java
+++ src/test/java/de/simbuildings/tilemapper/variations/SlabBlockStateTest.java
@@ -1,27 +1,27 @@
 package de.simbuildings.tilemapper.variations;
 
+import de.simbuildings.tilemapper.junit.StubResourcepackParameterResolver;
 import de.simbuildings.tilemapper.resourcepack.Resource;
 import de.simbuildings.tilemapper.resourcepack.Resourcepack;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.nio.file.Path;
 import java.util.Set;
 import java.util.function.Supplier;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
 
+@ExtendWith(StubResourcepackParameterResolver.class)
 class SlabBlockStateTest {
 
     @Test
-    @DisplayName("should return json for generic slab with single variant")
-    void shouldReturnJsonForGenericSlabWithSingleVariant() {
+    @DisplayName("should return json for slab with single variant")
+    void shouldReturnJsonForSlabWithSingleVariant(Resourcepack resourcepack) {
         // given
-        Resourcepack resourcepack = createAnyResourcepack();
         Resource resource = new Resource(resourcepack, "sandstone", "sandstone");
         Set<Variant.Builder> variants = Set.of(new Variant.Builder(resource));
-
         Supplier<BlockState> blockstateFactory = new SlabBlockStateFactory(variants);
 
         // when
@@ -43,11 +43,44 @@ class SlabBlockStateTest {
                             }
                           }
                         }
-                        """
-                ));
+                        """)
+        );
     }
 
-    private Resourcepack createAnyResourcepack() {
-        return new Resourcepack("default", Path.of("test"));
+    @Test
+    @DisplayName("should return json for slab with multiple variants")
+    void shouldReturnJsonForSlabWithMultipleVariants(Resourcepack resourcepack) {
+        // given
+        Resource sandstoneOne = new Resource(resourcepack, "sandstone", "sandstone1");
+        Resource sandstoneTwo = new Resource(resourcepack, "sandstone", "sandstone2");
+        Set<Variant.Builder> variants = Set.of(
+                new Variant.Builder(sandstoneOne),
+                new Variant.Builder(sandstoneTwo));
+        Supplier<BlockState> blockStateFactory = new SlabBlockStateFactory(variants);
+
+        // when
+        BlockState blockState = blockStateFactory.get();
+
+        // then
+        assertThatJson(blockState).isEqualTo(
+                json("""
+                        {
+                          "variants": {
+                            "type=bottom": [
+                                {"model": "minecraft:block/sandstone/slab/sandstone1_slab"},
+                                {"model": "minecraft:block/sandstone/slab/sandstone2_slab"}
+                            ],
+                            "type=double": [
+                                {"model": "minecraft:block/sandstone/sandstone1"},
+                                {"model": "minecraft:block/sandstone/sandstone2"}
+                            ],
+                            "type=top": [
+                                {"model": "minecraft:block/sandstone/slab/sandstone1_slab_top"},
+                                {"model": "minecraft:block/sandstone/slab/sandstone2_slab_top"}
+                            ]
+                          }
+                        }
+                        """)
+        );
     }
 }
