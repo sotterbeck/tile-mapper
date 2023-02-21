5e2664e930baa32e3b8fad75095370f4a48eda97
simbuildings <simon.otterbeck@gmail.com>
2023-02-21 15:48:00 +0100

    refactor: remove SCENE_HEIGHT

--- gui/src/main/java/de/simbuildings/tilemapper/gui/TileMapperApp.java
+++ gui/src/main/java/de/simbuildings/tilemapper/gui/TileMapperApp.java
@@ -13,7 +13,6 @@ import java.util.List;
 public class TileMapperApp extends Application {
 
     public static final int SCENE_WIDTH = 400;
-    public static final int SCENE_HEIGHT = 893;
     private static final TileMapperAppComponent LAUNCHER = DaggerTileMapperAppComponent.create();
 
     public static void main(String[] args) {
