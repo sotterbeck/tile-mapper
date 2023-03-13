95c084e094aac5da359bb0579f0466ff2ca80bdd
simbuildings <simon.otterbeck@gmail.com>
2023-03-13 21:22:37 +0100

    chore: added Mockito to test dependencies

--- buildSrc/src/main/kotlin/de.simbuildings.tilemapper.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/de.simbuildings.tilemapper.java-conventions.gradle.kts
@@ -19,6 +19,8 @@ repositories {
 dependencies {
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.mockito:mockito-core:5.2.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.2.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
